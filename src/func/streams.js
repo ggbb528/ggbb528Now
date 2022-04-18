8f6e15fcddc3931f6b5b8a011febcef032efea74
Jin Chen <bluemex.jin@gmail.com>
2022-04-18 17:37:03 +0800

    refresh token

--- src/func/streams.js
+++ src/func/streams.js
@@ -1,8 +1,9 @@
 module.exports = () => {
   // Variable
-  var accessToken = 'gs2cxwq056jcelg0knaw6gl4pxnygd';
-  var clientId = '3cppb0bah5pvz8p27qy1c5i3zbfwg7';
+  var clientId = 'gwku24kcuvzugy1tw91leenhku1133';
+  var client_secret = '3nlfof3bmonndmp2y1gobh0byk40l9';
   var twitchAPIUrl = 'https://api.twitch.tv/helix';
+  var twitchIDUrl = 'https://id.twitch.tv/oauth2';
 
   const Icons = {
     offline: {
@@ -19,35 +20,76 @@ module.exports = () => {
     },
   };
 
-  const checkStreams = () => {
-    var url = new URL(twitchAPIUrl + '/streams');
-    searchParams = new URLSearchParams({ user_login: 'ggbb528' });
-    // when support multi channel
-    // searchParams.append('user_login', 'muse_tw');
+  const refreshAccessToken = () => {
+    var url = new URL(twitchIDUrl + '/token');
 
-    url.search = searchParams.toString();
+    let formData = new FormData();
+    formData.append('client_id', clientId);
+    formData.append('client_secret', client_secret);
+    formData.append('grant_type', 'client_credentials');
+
     var option = {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + accessToken,
-        'Client-ID': clientId,
-      },
+      method: 'POST',
+      body: formData
     };
-
     fetch(url, option)
       .then((response) => response.json())
       .then((response) => {
-        if (response.data.length > 0) {
-          //   chrome.action.setBadgeBackgroundColor({ color: '#ff0000' });
-          //   chrome.action.setBadgeText({ text: 'Live' });
-          chrome.action.setIcon({ path: Icons.online });
-          chrome.storage.local.set({ ggbb528Open: true });
-        } else {
-          //   chrome.action.setBadgeText({ text: '' });
-          chrome.action.setIcon({ path: Icons.offline });
-          chrome.storage.local.set({ ggbb528Open: false });
+        chrome.storage.local.set({
+          twitchAccessToken:{
+            accessToken: response.access_token, 
+            expired: new Date().getTime() + response.expires_in
+          }
+        });
+    });
+  }
+
+  const validateToken = () => {
+    chrome.storage.local.get({
+      twitchAccessToken:{accessToken: "", expired: ""}
+    }, function(items) {
+        // local not token or token expired less in 1 day.
+        if (items.twitchAccessToken.accessToken == "" || items.twitchAccessToken.expired - new Date().getTime() < 86400) {
+          refreshAccessToken();
         }
-      });
+    });
+  }
+
+  const checkStreams = () => {
+    validateToken();
+    
+    // when support multi channel
+    // searchParams.append('user_login', 'muse_tw');
+    chrome.storage.local.get({
+      twitchAccessToken:{accessToken: "", expired: ""}
+    }, function(items) {
+      accessToken = items.twitchAccessToken.accessToken;
+
+      var url = new URL(twitchAPIUrl + '/streams');
+      searchParams = new URLSearchParams({ user_login: 'ggbb528' });
+
+      url.search = searchParams.toString();
+      var option = {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + accessToken,
+          'Client-ID': clientId,
+        },
+      };
+
+      fetch(url, option)
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.data && response.data.length > 0) {
+            chrome.action.setIcon({ path: Icons.online });
+            chrome.storage.local.set({ ggbb528Open: true });
+          } else {
+            chrome.action.setIcon({ path: Icons.offline });
+            chrome.storage.local.set({ ggbb528Open: false });
+          }
+        });
+
+    });
   };
 
   return {
