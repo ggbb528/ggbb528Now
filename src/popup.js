d9eb791700c52d56ea294c4e95aaa3cdb907a52e
stealwater <water2924@gmail.com>
2022-04-21 17:41:22 +0800

    fixed icon

--- src/popup.js
+++ src/popup.js
@@ -9,6 +9,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
 
 import './popup.css';
