0c856a7a46ea5d82247e2f43ce4c30e4d9d45454
stealwater <water2924@gmail.com>
2023-02-25 19:27:53 +0800

    Update TabRanking.tsx

--- src/pages/popup/components/tab-contents/TabRanking.tsx
+++ src/pages/popup/components/tab-contents/TabRanking.tsx
@@ -1,61 +1,13 @@
 import { Constants } from '@src/configs/constants';
 import React, { useRef, useState } from 'react';
-import useOPGGSpectates from '../../hooks/useOPGGSpectates';
 import { Account } from '../../models/account-type';
 import Pill from '../Pill';
 import RankingTable from '../RankingTable';
-
-interface ServerOptionButtonProps {
-  className?: string;
-  active?: boolean;
-  setAccount: React.Dispatch<React.SetStateAction<Account>>;
-  account: {
-    url: string;
-    summoner_id: string;
-    server: string;
-    account_id: string;
-  };
-}
-
-function ServerOptionButton({
-  className = '',
-  active,
-  account,
-  setAccount,
-}: ServerOptionButtonProps) {
-  const buttonEndRef = useRef<HTMLDivElement>(null);
-  const scrollToBtn = () => {
-    buttonEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  const onClickBtn = () => {
-    setAccount({
-      server: account.server as 'kr' | 'tw',
-      summonerId: account.summoner_id,
-    });
-    scrollToBtn();
-  };
-  return (
-    <li
-      className="nav-item flex-auto text-center cursor-pointer "
-      role="presentation"
-    >
-      <a
-        className={`nav-link w-full block font-medium text-xs leading-tight uppercase rounded px-6 py-2 focus:outline-none focus:ring-0 
-        ${active ? 'active' : ''} ${className}`}
-        role="tab"
-        aria-selected="true"
-        onClick={onClickBtn}
-      >
-        <div className="flex whitespace-nowrap gap-1 items-center">
-          <Pill bgColor="bg-yellow-500">{account.server.toUpperCase()}</Pill>{' '}
-          {account.account_id}
-          <div ref={buttonEndRef}></div>
-        </div>
-      </a>
-    </li>
-  );
-}
+import {
+  faChevronRight,
+  faChevronLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function ServerOptionButtons({
   account,
@@ -64,22 +16,86 @@ function ServerOptionButtons({
   account: Account;
   setAccount: React.Dispatch<React.SetStateAction<Account>>;
 }) {
+  const menuContainerRef = useRef<HTMLDivElement>(null);
+  const currentBtnRefs = useRef<(HTMLLIElement | null)[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const maxIndex = Constants.OPGG_ACCOUNTS.length;
+
+  const scrollToIndex = (index: number) => {
+    let nextIndex = index;
+    if (nextIndex >= maxIndex) nextIndex -= maxIndex;
+    if (nextIndex < 0) nextIndex += maxIndex;
+
+    currentBtnRefs.current[nextIndex]?.scrollIntoView({
+      inline: 'start',
+      block: 'nearest',
+      behavior: 'smooth',
+    });
+
+    setCurrentIndex(nextIndex);
+  };
+
   return (
-    <div className="py-1 px-2 overflow-x-auto scrollbar-hide">
-      <ul
-        className="nav nav-pills flex flex-row list-none gap-2"
-        id="pills-tabFill"
-        role="tablist"
+    <div className="relative overflow-hidden flex mx-1">
+      {currentIndex > 0 && (
+        <div
+          className={`flex items-center left-0 top-0 bottom-0 cursor-pointer`}
+          onClick={() => scrollToIndex(currentIndex - 1)}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} className="p-1" />
+        </div>
+      )}
+      <div
+        className="py-1 px-2 overflow-x-auto scrollbar-hide"
+        ref={menuContainerRef}
       >
-        {Constants.OPGG_ACCOUNTS.map((accountInfo) => (
-          <ServerOptionButton
-            key={accountInfo.summoner_id}
-            active={account.summonerId === accountInfo.summoner_id}
-            account={accountInfo}
-            setAccount={setAccount}
-          ></ServerOptionButton>
-        ))}
-      </ul>
+        <ul
+          className="nav nav-pills flex flex-row list-none gap-2"
+          id="pills-tabFill"
+          role="tablist"
+        >
+          {Constants.OPGG_ACCOUNTS.map((accountInfo, i) => (
+            <li
+              key={accountInfo.summoner_id}
+              className="nav-item flex-auto text-center cursor-pointer "
+              role="presentation"
+              ref={(el) => (currentBtnRefs.current[i] = el)}
+            >
+              <a
+                className={`nav-link w-full block font-medium text-xs leading-tight uppercase rounded px-6 py-2 focus:outline-none focus:ring-0 
+               ${
+                 account.summonerId === accountInfo.summoner_id ? 'active' : ''
+               }`}
+                role="tab"
+                aria-selected="true"
+                onClick={() => {
+                  setAccount({
+                    server: accountInfo.server as 'kr' | 'tw',
+                    summonerId: accountInfo.summoner_id,
+                  });
+
+                  scrollToIndex(i);
+                }}
+              >
+                <div className="flex whitespace-nowrap gap-1 items-center">
+                  <Pill bgColor="bg-yellow-500">
+                    {accountInfo.server.toUpperCase()}
+                  </Pill>
+                  {accountInfo.account_id}
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {currentIndex < maxIndex - 1 && (
+        <div
+          className="flex items-center right-0 top-0 bottom-0 cursor-pointer"
+          onClick={() => scrollToIndex(currentIndex + 1)}
+        >
+          <FontAwesomeIcon icon={faChevronRight} className="p-1" />
+        </div>
+      )}
     </div>
   );
 }
@@ -116,12 +132,6 @@ export default function TabRanking() {
     summonerId: Constants.OPGG_ACCOUNTS[0].summoner_id,
   });
 
-  const { data, error } = useOPGGSpectates({
-    server: Constants.OPGG_ACCOUNTS[0].server as 'kr' | 'tw',
-    summonerId: Constants.OPGG_ACCOUNTS[0].summoner_id,
-  });
-
-  console.log('err', error);
   return (
     <>
       <ServerOptionButtons setAccount={setAccount} account={account} />
